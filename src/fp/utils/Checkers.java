550921d7f0f5f11f5ee809d5612adb8e9b8f84b3
tuiqpan <116158391+tuiqpan@users.noreply.github.com>
2023-03-12 23:06:06 +0100

    Add files via upload

---
+++ src/fp/utils/Checkers.java
@@ -0,0 +1,28 @@
+package fp.utils;
+
+
+public class Checkers {
+
+
+	public static void check(String textoRestriccion, Boolean condicion) {
+		if (!condicion) {
+			throw new IllegalArgumentException(
+					Thread.currentThread().getStackTrace()[2].getClassName() +
+					"." + 
+					Thread.currentThread().getStackTrace()[2].getMethodName() +
+					": " + 
+					textoRestriccion);
+		}
+	}
+	public static void checkNoNull(Object... parametros) {
+		for (int i = 0; i < parametros.length; i++) {
+			if (parametros[i] == null) {
+				throw new IllegalArgumentException(
+						Thread.currentThread().getStackTrace()[2].getClassName() +
+						"." + 
+						Thread.currentThread().getStackTrace()[2].getMethodName() +
+						": el par�metro " + (i + 1) + " es nulo");
+			}
+		}
+	}
+}
